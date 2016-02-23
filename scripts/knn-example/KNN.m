b2afcc1f6d920a430aae8f617e28f4d4fbf8bb4f
Diogenes Laertius <diogeneslaertiussof@gmail.com>
2016-02-23 11:25:56 -0300

    Accuracy in KNN

--- scripts/knn-example/KNN.m
+++ scripts/knn-example/KNN.m
@@ -1,11 +1,20 @@
 load cmm.data
 
-entidades = cmm(1:1470,1:9);
-classes = cmm(1:1470, 10);
+entidades = cmm(1:1326,1:9);
+classes = cmm(1:1326, 10);
 
-classificador = fitcknn(entidades,classes,'Distance','cosine', 'NumNeighbors',3);
+classificador = fitcknn(entidades,classes,'Distance','euclidean', 'NumNeighbors',41);
 
-teste1 = cmm(1471:1473, 1:9);
-teste1_classe = cmm(1471:1473, 10);
+teste1 = cmm(1327:1473, 1:9);
+teste1_classe = cmm(1327:1473, 10);
 
-predicao = predict(classificador,teste1);
+[label score] = predict(classificador,teste1);
+
+z = 0;
+for x = 1:size(label)
+    if (label(x) == teste1_classe(x))
+        z = z + 1;
+    end
+end
+
+z = z/147;
