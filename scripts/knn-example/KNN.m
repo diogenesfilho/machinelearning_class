951973629b98bec381e005630d94c71f80910cfc
Diogenes Laertius <diogeneslaertiussof@gmail.com>
2016-03-16 09:12:47 -0300

    Decision Tree

--- scripts/knn-example/KNN.m
+++ scripts/knn-example/KNN.m
@@ -3,7 +3,7 @@ load cmm.data
 entidades = cmm(1:1326,1:9);
 classes = cmm(1:1326, 10);
 
-classificador = fitcknn(entidades,classes,'Distance','euclidean', 'NumNeighbors',13);
+classificador = fitcknn(entidades,classes,'Distance','euclidean', 'NumNeighbors',35);
 
 teste1 = cmm(1327:1473, 1:9);
 teste1_classe = cmm(1327:1473, 10);
